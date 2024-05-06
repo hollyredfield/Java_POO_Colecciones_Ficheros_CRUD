58e774ee1946f1006aaac5ea6d03490577f0a348
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2024-05-06 10:23:12 +0100

    save

---
+++ HerenciaJava/src/com/herencia/Animal.java
@@ -0,0 +1,10 @@
+package com.herencia;
+
+// Definimos la clase Animal
+public class Animal {
+    // Método comer de la clase Animal
+    public void comer() {
+        // Imprime en consola "El animal come"
+        System.out.println("El animal come");
+    }
+}
